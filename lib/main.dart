d6ed33ce0020e9a37ed7d8d8e5e50517df6908a7
Ishan Sharma <ishandeveloper@outlook.com>
2019-12-29 17:40:13 +0530

    Upload Code

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+
+   new Material(
+     color:Colors.lightGreenAccent,
+     child: new Center(
+       child: new Text(
+         "Hello There, Ishan Sharma",
+         textDirection: TextDirection.ltr,
+       ),
+     ),
+
+
+
+   )
+  );
+}

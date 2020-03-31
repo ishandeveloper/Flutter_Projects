2e77ad9921496b1b153ac8439432c71c7446afcf
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-01 01:16:37 +0530

    main.dart

---
+++ 12 StreamBuilder/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:ishandeveloper_app/pages/loginpage.dart';
+import 'package:ishandeveloper_app/pages/streamBuilder_hp.dart';
+import 'package:ishandeveloper_app/utils/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  Constants.prefs= await SharedPreferences.getInstance();
+  runApp(MyApp());
+  
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home:Constants.prefs.getBool("loggedIn")==true?HomePageSB():LoginPage(),
+        theme: ThemeData(
+          primarySwatch: Colors.purple
+          ),
+      
+    );
+  }
+}
